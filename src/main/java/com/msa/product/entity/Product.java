0c6be2e1d3ad57ce54d76f77844842d09b5515f9
chupdachups <chupdachups@gmail.com>
2022-04-28 02:07:44 +0900

    blank template commit

--- src/main/java/com/msa/product/entity/Product.java
+++ src/main/java/com/msa/product/entity/Product.java
@@ -1,14 +1,9 @@
 package com.msa.product.entity;
 
-import java.util.Date;
-
-import javax.persistence.*;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,38 +12,5 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
-
-	@Id
-	@Column(name = "product_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(nullable = false)
-	private String productName;
-	
-	@Column(nullable = false)
-	private String address;
-	
-	@Column(nullable = false)
-	private String detailAddress;
-	
-	@Column(nullable = false)
-	private Long price;
-	
-	@CreationTimestamp
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdAt;
-
-    @UpdateTimestamp
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date updatedAt;
-	
-	@Builder
-	public Product(String productName, String address, String detailAddress, Long price) {
-		this.productName = productName;
-		this.address = address;
-		this.detailAddress = detailAddress;
-		this.price = price;
-	}
 	
 }
